fc447a789286d6f2c35a17167814f94e7e87ed8b
Ipsita <baralipsita592@gmail.com>
2024-04-30 22:16:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let testimonial = document.querySelector(".testimo");
+
+
+$('testimonial').slick({
+    infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows:false,
+  dots: true,
+  Steed:1000,
+  autoplay:2000
+})
